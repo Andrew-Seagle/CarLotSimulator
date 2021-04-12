8b529ecd67cb84dde7ae2c96c235318cc776852d
Andrew-Seagle <andrewseagle93@gmail.com>
2021-04-12 15:50:06 -0400

    formatting

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -38,17 +38,7 @@ namespace CarLotSimulator
                 car.MakeHonkNoise();
             }
 
-            //*************BONUS*************//
-
-            // Set the properties utilizing the 3 different ways we learned about, one way for each car
-
-            //*************BONUS X 2*************//
-
-            //Create a CarLot class
-            //It should have at least one property: a List of cars
-            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
-
             foreach (Car car in CarLot.ListOfCars)
             {
                 Console.WriteLine("\n" +
